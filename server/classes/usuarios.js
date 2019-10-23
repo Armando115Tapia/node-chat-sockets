c0313ddf702cbc692f5adbf5fbc8ee2721d9e798
Armando Tapia <armandotapia115@hotmail.com>
2019-10-23 15:19:45 -0500

    Creacion de la clase usuarios

---
+++ server/classes/usuarios.js
@@ -0,0 +1,15 @@
+class Usuarios {
+  constructor() {
+    this.personas = [];
+  }
+
+  agregarPersona(id, nombre) {
+    let persona = { id, nombre };
+    this.personas.push(persona);
+    return this.personas;
+  }
+}
+
+module.exports = {
+  Usuarios
+};
